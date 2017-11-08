f9562d327a9749cdde0aa14133a6ca1dd409b308
Kristof Ballet <kristofb@intouch.be>
2017-11-08 21:57:35 +0100

    Ex 11 done

--- src/exercises/chapter6/Ex1_5_Lotto.java
+++ src/exercises/chapter6/Ex1_5_Lotto.java
@@ -5,11 +5,12 @@ import java.util.*;
  */
 public class Ex1_5_Lotto {
     public static void main(String[] args) {
-        Random rand = new Random();
+        long seed = System.currentTimeMillis();
+        Random rand = new Random(seed);
         int i = 1;
         while (i <= 6 ) {
             int getal = rand.nextInt(45);
-            getal = getal + 1;
+            getal++; //= getal + 1;
             System.out.println("Getal " +i+" is "+getal);
             i++;
         }
