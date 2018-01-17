f120133fa462434320e68cdb4c3b5f34f979ad9a
Kristof Ballet <kristofb@intouch.be>
2018-01-17 21:50:18 +0100

    Lesson 1 advanced done

--- src/exercises/chapter10/Square.java
+++ src/exercises/chapter10/Square.java
@@ -6,6 +6,30 @@ import exercises.chapter8.Rectangle;
  * Created by KristofB on 17/01/2018.
  */
 public class Square extends Rectangle {
+
+    private static int count = 0;
+    {count++;}
+
+    public static int getCount() {
+        return count;
+    }
+
+    public Square(){
+        this(0);
+    }
+
+    public Square (int s){
+    this(s, 0, 0);
+    }
+
+    public Square (int s, int x, int y) {
+        super(s, s, x, y);
+    }
+
+    public Square(Square square){
+        this(square.getSide(), square.getX(), square.getY());
+    }
+
     @Override
     public void setHeight(int h) {
         setSide(h);
@@ -23,4 +47,7 @@ public class Square extends Rectangle {
         super.setHeight(side);
         super.setWidth(side);
     }
+
+
+
 }
--- src/exercises/chapter10/SquareApp.java
+++ src/exercises/chapter10/SquareApp.java
@@ -1,5 +1,7 @@
 package exercises.chapter10;
 
+import exercises.chapter8.Rectangle;
+
 /**
  * Created by KristofB on 17/01/2018.
  */
@@ -20,6 +22,25 @@ public class SquareApp {
         System.out.println(vierkant.getSide());
         System.out.println("De oppervlakte is: " + vierkant.getArea());
         System.out.println("De omtrek is: " + vierkant.getPerimeter());
+
+        Square test = new Square(30, 40,40);
+        System.out.println(test.getSide());
+        System.out.println("De oppervlakte is: " + test.getArea());
+        System.out.println("De omtrek is: " + test.getPerimeter());
+
+        Square meuh = new Square(test);
+        System.out.println(test.getSide());
+        System.out.println("De oppervlakte is: " + meuh.getArea());
+        System.out.println("De omtrek is: " + meuh.getPerimeter());
+        meuh.setSide(100);
+        System.out.println("De oppervlakte is: " + meuh.getArea());
+        System.out.println("De omtrek is: " + meuh.getPerimeter());
+
+        System.out.println("-----");
+        System.out.println("aantal vierkanten: " + meuh.getCount());
+        Rectangle rechthoek1 = new Rectangle();
+        System.out.println("aantal rechthoeken: " + rechthoek1.getCount());
+        System.out.println("aantal vierkanten: " + meuh.getCount());
 
     }
 }
